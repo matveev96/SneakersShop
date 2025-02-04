9878bcbf307a32d0c923e2a079988719a569fd68
matveev96 <sasha.matveev1996@icloud.com>
2025-02-04 18:41:31 +0300

    feat: update from HashRouter to RouterProvider

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter, HashRouter} from "react-router-dom";
+import {RouterProvider} from "react-router-dom";
+import {router} from "./routes/router";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-    <HashRouter>
-        <App/>
-    </HashRouter>
+    <RouterProvider router={router}/>
 );
 
 // If you want to start measuring performance in your app, pass a function
