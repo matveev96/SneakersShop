230bd39a142186b562b98ab332aea6dbd9a9f147
matveev96 <sasha.matveev1996@icloud.com>
2025-02-04 20:05:13 +0300

    refactor: refactor footer

--- src/App.tsx
+++ src/App.tsx
@@ -4,37 +4,22 @@ import {S} from './components/pages/_styles';
 import {PATH} from "./routes/router";
 
 function App() {
+
     return (
         <div>
             <S.Header><h1>HEADER</h1></S.Header>
             <S.Body>
                 <S.Navigation>
-                    <S.NavWrapper>
-                        <NavLink to={PATH.ADIDAS}>
-                            Adidas
-                        </NavLink>
-                    </S.NavWrapper>
-                    <S.NavWrapper>
-                        <NavLink to={PATH.PUMA}>
-                            Puma
-                        </NavLink>
-                    </S.NavWrapper>
-                    <S.NavWrapper>
-                        <NavLink to={PATH.ABIBAS}>
-                            Abibas
-                        </NavLink>
-                    </S.NavWrapper>
-                    <S.NavWrapper>
-                        <NavLink to={PATH.PRICES}>
-                            Цены для оптовиков
-                        </NavLink>
-                    </S.NavWrapper>
+                    <S.NavWrapper><NavLink to={PATH.ADIDAS}>Adidas</NavLink></S.NavWrapper>
+                    <S.NavWrapper><NavLink to={PATH.PUMA}>Puma</NavLink></S.NavWrapper>
+                    <S.NavWrapper><NavLink to={PATH.ABIBAS}>Abibas</NavLink></S.NavWrapper>
+                    <S.NavWrapper><NavLink to={PATH.PRICES}>Цены для оптовиков</NavLink></S.NavWrapper>
                 </S.Navigation>
                 <S.Content>
                     <Outlet/>
                 </S.Content>
             </S.Body>
-            <S.Footer>abibas 2023</S.Footer>
+            <S.Footer>PODKRADYLI 2024</S.Footer>
         </div>
     );
 }
