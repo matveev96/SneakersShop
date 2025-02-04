957eedb3307b0b7b0ef4da54abebd2a35759fa1d
matveev96 <sasha.matveev1996@icloud.com>
2025-02-04 18:40:23 +0300

    feat: add router.tsx in project

---
+++ src/routes/router.tsx
@@ -0,0 +1,49 @@
+import * as React from "react";
+import {createBrowserRouter,} from "react-router-dom";
+import App from "../App";
+import {Error404} from "../components/pages/Error404";
+import {Adidas} from "../components/pages/Adidas";
+import {Puma} from "../components/pages/Puma";
+import {Abibas} from "../components/pages/Abibas";
+import {Model} from "../components/pages/Model";
+import {Prices} from "../components/pages/Prices";
+
+export const PATH = {
+    ADIDAS: 'adidas',
+    PUMA: 'puma',
+    ABIBAS: 'abibas',
+    PRICES: 'prises',
+    MODEL: ':model/:id',
+} as const
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+        errorElement: <Error404/>,
+        children: [
+            {
+                path: PATH.ADIDAS,
+                element: <Adidas/>,
+            },
+            {
+                path: PATH.PUMA,
+                element: <Puma/>,
+            },
+            {
+                path: PATH.ABIBAS,
+                element: <Abibas/>,
+            },
+            {
+                path: PATH.PRICES,
+                element: <Prices/>,
+            },
+            {
+                path: PATH.MODEL,
+                element: <Model/>,
+            },
+
+        ]
+    }
+]);
+
