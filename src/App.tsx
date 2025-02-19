ec02de643482b0041093a88695628943e0cc5ef2
matveev96 <sasha.matveev1996@icloud.com>
2025-02-19 09:41:21 +0300

    feat: add Links on Previous Page and Main Page (ADIDAS)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import {NavLink, Outlet} from "react-router-dom";
+import {Link, NavLink, Outlet, useNavigate} from "react-router-dom";
 import {S} from './components/pages/_styles';
 import {PATH} from "./routes/router";
+import styles from "./components/Site.module.css"
 
 function App() {
+
+    const navigate = useNavigate()
+
+    const navigateHandler = (e: React.MouseEvent<HTMLAnchorElement>) => {
+        e.preventDefault()
+        navigate(-1)
+    }
 
     return (
         <div>
@@ -17,6 +25,10 @@ function App() {
                     <S.NavWrapper><NavLink to={PATH.PROTECTED_PAGE}>Protected Page</NavLink></S.NavWrapper>
                 </S.Navigation>
                 <S.Content>
+                    <div className={styles.HorizontalNavigation}>
+                        <Link className={styles.LinkLikeButton} to={PATH.ADIDAS}> Main Page (ADIDAS) </Link>
+                        <Link className={styles.ButtonLikeLink} onClick={navigateHandler} to={'..'}> Previous Page </Link>
+                    </div>
                     <Outlet/>
                 </S.Content>
             </S.Body>
